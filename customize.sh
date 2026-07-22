b1ad6d05f2191330c63cd03b434991b14adaf989
xc <48278853+ojbkxc@users.noreply.github.com>
2026-07-22 23:24:56 +0800

    编译

--- customize.sh
+++ customize.sh
@@ -3,17 +3,20 @@ PROPFILE=false
 POSTFSDATA=false
 LATESTARTSERVICE=true
 
-# 复制二进制到模块根目录（参照 cloudflare-Magisk / lucky_Magisk）
-if [ -f "$MODPATH/system/bin/taosync" ]; then
-  cp "$MODPATH/system/bin/taosync" "$MODPATH/taosync_bin"
+# 设置执行权限
+if [ -f "$MODPATH/system/bin/python3.11-android" ]; then
+  set_perm $MODPATH/system/bin/python3.11-android 0 0 0755
+  ui_print "- Python 二进制文件就绪"
+else
+  ui_print "! 警告：未找到 Python 二进制文件"
 fi
 
-# 设置执行权限
-if [ -f "$MODPATH/taosync_bin" ]; then
-  set_perm $MODPATH/taosync_bin 0 0 0755
-  ui_print "- taoSync 二进制文件就绪"
+# 设置 taosync 目录权限
+if [ -d "$MODPATH/system/lib/taosync" ]; then
+  set_perm_recursive $MODPATH/system/lib/taosync 0 0 0755 0644
+  ui_print "- taoSync 源码目录就绪"
 else
-  ui_print "! 警告：未找到 taoSync 二进制文件，模块可能无法正常工作"
+  ui_print "! 警告：未找到 taosync 源码目录"
 fi
 
 ui_print "----------------------------------"
